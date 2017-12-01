1425c54533b703892debaffcf85e2fc4c876ee2a
Andrey Steshenko <andreysteshenko@cloud.upwork.com>
2017-12-01 04:20:27 +0300

    a

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.substring(0, 2) == ‘леха’){
-      message.reply('САСИ');
+    if (message.substring(0, 2) == 'lexa'){
+      message.reply('саси');
     }
 });
 
